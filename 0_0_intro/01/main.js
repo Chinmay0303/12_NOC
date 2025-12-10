9a56cab366581f5fb25cc02eb1f89dcb94eecbfa
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-12-10 13:57:10 +0530

    create main.js

---
+++ 0_0_intro/01/main.js
@@ -0,0 +1,61 @@
+const log = console.log;
+
+log("main");
+
+const mainContainer = document.querySelector('.main');
+
+// log(mainContainer)
+const canvas = document.querySelector(".my-canvas");
+const ctx = canvas.getContext('2d');
+
+// log(ctx);
+
+const mcWidth = mainContainer.clientWidth;
+const mcHeight = mainContainer.clientHeight;
+
+log(mcWidth,mcHeight);
+// log(canvas.width, canvas.height);
+
+canvas.width = 8/10 * mcWidth;
+canvas.height = 9.8/10 * mcHeight;
+
+// log(canvas.width, canvas.height);
+
+function drawCircle(){
+
+    ctx.beginPath();
+    const radius = Math.floor(Math.random() * 20) + 10;
+    const rand_height = Math.random() * (canvas.height - 4*radius) + 2*radius;
+    const rand_width = Math.random() * (canvas.width - 4*radius) + 2*radius;
+
+    if (canvas.width < 4 * radius || canvas.height < 4 * radius) {
+        // Skip drawing if the canvas is too small for the circle
+        return;
+    }
+
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    ctx.arc(rand_width, rand_height, radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = `rgb(${red}, ${green}, ${blue})`;
+    ctx.fillStyle = `rgba(${red}, ${green}, ${blue}, 0.1)`;
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+}
+
+// drawCircle();
+
+// for(let i=0;i<51;i++){
+//     drawCircle();
+// }
+
+i = 0
+function animate(){
+    if((i++)%5 === 0)
+        drawCircle();
+    requestAnimationFrame(animate);
+}
+
+animate();
